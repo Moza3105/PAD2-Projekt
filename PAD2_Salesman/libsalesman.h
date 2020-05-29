e80a7955bfb49be16a5d7b2f9635302af4d55b5a
Hamza Shehadeh <mr19b008@technikum-wien.at>
2020-05-29 18:34:02 +0200

    city struct, Selection Sort, makecity, printcity

--- PAD2_Salesman/libsalesman.h
+++ PAD2_Salesman/libsalesman.h
@@ -1,1 +1,195 @@
+#include <string.h>
 
+typedef struct City{
+    char*  name;
+    char* ascii;
+    float lat;
+    float lng;
+    char* country;
+    char* iso2;
+    char* iso3;
+    char* adminname;
+    char* capital;
+    int population;
+    int id;
+}city;
+
+
+city makecity(char* name, char* ascii, float lat, float lng, char* country, char* iso2, char* iso3, char* adminname, char* capital, int population, int id)
+{
+    city tmp;
+    tmp.name = name;
+    tmp.ascii = ascii;
+    tmp.lat = lat;
+    tmp.lng = lng;
+    tmp.country = country;
+    tmp.iso2 = iso2;
+    tmp.iso3 = iso3;
+    tmp.adminname = adminname;
+    tmp.capital = capital;
+    tmp.population = population;
+    tmp.id = id;
+    return tmp;
+}
+
+city printcity(city tmp)
+{
+    printf("%s, %s, %.5f, %.5f, %s, %s, %s, %s, %s, %d, %d\n", tmp.name, tmp.ascii, tmp.lat, tmp.lng, tmp.country, tmp.iso2, tmp.iso3, tmp.adminname, tmp.capital, tmp.population, tmp.id);
+    return tmp;
+}
+
+city* selectionsort(city* array, int length)
+{
+
+
+    char alphabet[26] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+    int min;
+    city placeholdercity;
+    int* value = malloc(length*sizeof(array[0]));
+    char* tmp;
+
+    for(int i = 0; i<length; i++)
+    {
+
+        tmp = array[i].name;
+            //printf("%c", tmp[0]);
+        for(int j = 0; j < 26; j++)
+        {
+
+            if(tmp[0]==alphabet[j])
+            {
+                //printf("%d  ", j);
+                value[i]=j;
+                break;
+            }
+            else if(j==25)
+            {
+                //printf("%d  ", 27);
+                value[i]=27;
+            }
+        }
+
+    }
+
+    for(int j = 0; j<length; j++)
+    {
+
+        min=j;
+
+        for(int i = j; i<length; i++)
+        {
+            if(value[i]<value[min])
+            {
+                min = i;
+            }
+        }
+        placeholdercity = array[j];
+        array[j]=array[min];
+        array[min]= placeholdercity;
+        value[min]=value[j];
+
+    }
+
+    printf("Sorted using Selection-Sort! ;-)\n");
+    return array;
+}
+//nicht benutzt aber vlt hilfreich
+void makearray(FILE *myfile)
+{
+    city* unsorted = malloc(7);
+    city Wien = makecity("Wien","ABCD",0,90,"Austria","2er","3er","adminland","Vienna",8000000,12345678);
+    printcity(Wien);
+
+    int j=0;
+    int l=1;
+    char c;
+    char* str;
+    str = malloc(sizeof(char));
+    fseek(myfile, 3, SEEK_SET);
+
+
+    while(1) {
+    c = fgetc(myfile);
+
+    if( feof(myfile) )
+    {
+        break ;
+    }
+
+        if(c=='\n')
+        {
+            printf("--< %d",l);
+            str[j]='\0';
+            j=0;
+
+
+
+            int len = strlen(str);
+
+            for(int i = 0; i < len; i++)
+            {
+                if(str[i] == '\"')
+                {
+                    for(int h = i; h < len; h++)
+                    {
+                        str[h] = str[h + 1];
+
+                    }
+                    len--;
+                    i--;
+                }
+            }
+
+            len = strlen(str);
+            for(int i = 0; i < len; i++)
+            {
+
+                if(str[i] == ',')
+                {
+                    for(int h = i; h < len; h++)
+                    {
+                        str[h] = str[h + 1];
+                    }
+                    len--;
+                    i--;
+
+                }
+                unsorted[l].name = str;
+            }
+
+
+
+        //printf("\n%s --ln:%d\n", str, l);
+        printf("citysize*l:%d\n", l*sizeof(city));
+        realloc(str, (1)*sizeof(char));
+
+        unsorted = (city*) realloc(unsorted,l*sizeof(city));
+        if (unsorted == NULL)
+        {
+            printf("NOOOOOO!!!");
+            return -1;
+        }
+
+        //printf("unsorted: %d\n",(sizeof(unsorted[7000])));
+
+        printf("check\n");
+        l++;
+
+
+
+        }
+
+        str[j]=c;
+        //printf("%c", c);
+        str = (char *) realloc(str, (j+2)*sizeof(char));
+        j++;
+
+   }
+
+   free(str);
+
+
+
+
+    //return unsorted;
+}
--- PAD2_Salesman/main.c
+++ PAD2_Salesman/main.c
@@ -4,6 +4,63 @@
 
 int main()
 {
+    FILE *worldcities = fopen("worldcities.txt", "r");
+
     printf("Hallo! Das ist das Testprogramm!\n");
+    if (NULL == worldcities)
+    {
+        printf("could not open file");
+        exit(-1);
+    }
+
+    city cities[19];
+    int number = sizeof(cities)/sizeof(cities[0]);
+    city* sortedcities;
+
+    // define 19 cities
+    {
+
+    cities[0] = makecity("Wien","ABCD",0,90,"Austria","2er","3er","adminland","Vienna",8000000,12345678);
+    cities[1] = makecity("Tokyo","Tokyo",35.6850,139.7514,"Japan","JP","JPN","Tōkyō","primary",35676000,1392685764);
+    cities[2] = makecity("New York","New York",40.6943,-73.9249,"United States","US","USA","New York","",19354922.0,1840034016);
+    cities[3] = makecity("Mexico City","Mexico City",19.4424,-99.1310,"Mexico","MX","MEX","Ciudad de México","primary",19028000,1484247881);
+    cities[4] = makecity("Mumbai","Mumbai",19.0170,72.8570,"India","IN","IND","Mahārāshtra","admin",18978000,1356226629);
+    cities[5] = makecity("São Paulo","Sao Paulo",-23.5587,-46.6250,"Brazil","BR","BRA","São Paulo","admin",18845000,1076532519);
+    cities[6] = makecity("Delhi","Delhi",28.6700,77.2300,"India","IN","IND","Delhi","admin",15926000,1356872604);
+    cities[7] = makecity("Shanghai","Shanghai",31.2165,121.4365,"China","CN","CHN","Shanghai","admin",14987000,1156073548);
+    cities[8] = makecity("Kolkata","Kolkata",22.4950,88.3247,"India","IN","IND","West Bengal","admin",14787000,1356060520);
+    cities[9] = makecity("Los Angeles","Los Angeles",34.1139,-118.4068,"United States","US","USA","California","",12815475.0,1840020491);
+    cities[10] = makecity("Dhaka","Dhaka",23.7231,90.4086,"Bangladesh","BD","BGD","Dhaka","primary",12797394,1050529279);
+    cities[11] = makecity("Buenos Aires","Buenos Aires",-34.6025,-58.3975,"Argentina","AR","ARG","Buenos Aires, Ciudad Autónoma de","primary",12795000,1032717330);
+    cities[12] = makecity("Karachi","Karachi",24.8700,66.9900,"Pakistan","PK","PAK","Sindh","admin",12130000,1586129469);
+    cities[13] = makecity("Cairo","Cairo",30.0500,31.2500,"Egypt","EG","EGY","Al Qāhirah","primary",11893000,1818253931);
+    cities[14] = makecity("Rio de Janeiro","Rio de Janeiro",-22.9250,-43.2250,"Brazil","BR","BRA","Rio de Janeiro","admin",11748000,1076887657);
+    cities[15] = makecity("Ōsaka","Osaka",34.7500,135.4601,"Japan","JP","JPN","Ōsaka","admin",11294000,1392419823);
+    cities[16] = makecity("Beijing","Beijing",39.9289,116.3883,"China","CN","CHN","Beijing","primary",11106000,1156228865);
+    cities[17] = makecity("Manila","Manila",14.6042,120.9822,"Philippines","PH","PHL","Manila","primary",11100000,1608618140);
+    cities[18] = makecity("Moscow","Moscow",55.7522,37.6155,"Russia","RU","RUS","Moskva","primary",10452000,1643318494);
+
+
+    }
+
+    for(int i = 0; i<19; i++)
+    {
+        printcity(cities[i]);
+    }
+
+    printf("\n--------\n");
+
+     sortedcities = selectionsort(cities,number);
+
+    printf("--------\n\n");
+
+    for(int i = 0; i<19; i++)
+    {
+        printcity(sortedcities[i]);
+    }
+
+
+
+    fclose(worldcities);
     return 0;
 }
